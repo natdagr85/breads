e2bb8da77a6a741834d8e5a4c958df499ac11e5d
natdagr85 <nathanmansoor1128@gmail.com>
2022-08-07 17:40:39 -0700

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -2,12 +2,12 @@ const express = require("express");
 const breads = express.Router();
 const Bread = require("../models/bread");
 
-//Index
+// INDEX
 breads.get("/", (req, res) => {
   res.render("Index", {
     breads: Bread,
+    title: "Index Page",
   });
-  // res.send(Bread)
 });
 
 //Show
